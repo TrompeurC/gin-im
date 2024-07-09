5061a7eaf6f923eb8dcfc2b7115b0b4b2823d316
TrompeurC <13604811047@163.com>
2024-07-09 23:46:47 +0800

    feat: gorm 日志

--- db/mysql_db.go
+++ db/mysql_db.go
@@ -8,6 +8,9 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
 )
 
 var MysqlDB *gorm.DB
@@ -15,6 +18,11 @@ var MysqlDB *gorm.DB
 func InitMysql() {
 	var mysqlConf = utils.Conf.Mysql
 	var sqlDSN = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", mysqlConf.Username, mysqlConf.Password, mysqlConf.Addr, mysqlConf.Database)
+	newLogger := logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+		SlowThreshold: time.Second,
+		Colorful:      true,
+		LogLevel:      logger.Info,
+	})
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:                       sqlDSN, // DSN data source name
 		DefaultStringSize:         256,    // string 类型字段的默认长度                                                                   // string 类型字段的默认长度
@@ -23,7 +31,7 @@ func InitMysql() {
 		DontSupportRenameColumn:   true,   // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
 		SkipInitializeWithVersion: false,  // 根据当前 MySQL 版本自动配置
 	}), &gorm.Config{
-		Logger:         logger.Default.LogMode(logger.Info),
+		Logger:         newLogger,
 		NamingStrategy: schema.NamingStrategy{SingularTable: false},
 	})
 	if err != nil {
